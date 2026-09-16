5fb06d98b5571875f57a6e6b04dc4c340cd780bf
XiaRao <2929710239@qq.com>
2026-09-15 21:54:48 -0400

    added Reduce method

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,18 +1,21 @@
 package lab1;
 
 public class Reduce {
-    static void main(String[] args) {
-        count = 0;
-        n = 100;
+    public static void main(String[] args) {
+        int steps = reduce(100);
+        System.out.println(steps);
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
         while (n != 0){
             if (n % 2 == 0){
                 n = n / 2;
-                count += 1;
             } else {
                 n -= 1;
-                count += 1;
             }
+            count += 1;
         }
-        System.out.println(count);
+        return count;
     }
 }
